ee4db8059b7df3fe69d5e107954012ed3e9c341c
Felix Nievelstein <felix.nievelstein@appside.me>
2019-01-08 22:09:07 +0100

    adding more pins

--- gpioservice.py
+++ gpioservice.py
@@ -5,6 +5,11 @@ import RPi.GPIO as GPIO
 P_Switch1 = 29 # switch 1
 P_Switch2 = 31 # switch 2
 P_Switch3 = 33 # switch 3
+P_Stop1 = 35 # stop 1
+P_Stop2 = 36 # stop 2
+P_Stop3 = 38 # stop 3
+P_Stop4 = 40 # stop 3
+
 
 def switchPin(pin):        
     if GPIO.input(pin):
@@ -18,6 +23,10 @@ def setup():
     GPIO.setup(P_Switch1, GPIO.OUT)
     GPIO.setup(P_Switch2, GPIO.OUT)
     GPIO.setup(P_Switch3, GPIO.OUT)
+    GPIO.setup(P_Stop1, GPIO.OUT)
+    GPIO.setup(P_Stop2, GPIO.OUT)
+    GPIO.setup(P_Stop3, GPIO.OUT)
+    GPIO.setup(P_Stop4, GPIO.OUT)
 
 
 def receivedMessage(message):
