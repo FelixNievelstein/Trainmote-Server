d5c8da17860b03772ee811e88ddd6276f4fc35c8
FelixNievelstein <info@felix-nievelstein.de>
2019-03-27 07:37:58 +0100

    fix path config

--- configController.py
+++ configController.py
@@ -8,8 +8,7 @@ class ConfigController():
         return None
 
     def loadSettings(self):
-        files = ['content/settings.ini']
+        files = ['./content/settings.ini']
         if self.parser.read(files) == 1:
             return files[0]
-
         return None
