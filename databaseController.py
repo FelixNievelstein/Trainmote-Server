678319b89290045214d5c56fa905873a6183c044
FelixNievelstein <info@felix-nievelstein.de>
2019-03-29 17:47:54 +0100

    removing default ini, creating inital database

--- databaseController.py
+++ databaseController.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 from configController import ConfigController
 
 class DatabaseController():
@@ -8,8 +9,10 @@ class DatabaseController():
     def openDatabase(self):
         config = ConfigController()
         dbPath = config.getDataBasePath()
-        print(dbPath)
         if dbPath is not None:
+            if not os.path.exists(dbPath):
+                self.createInitalDatabse(dbPath)
+            
             try:
                 self.conn = sqlite3.connect(dbPath)
                 print(self.conn)
@@ -20,6 +23,16 @@ class DatabaseController():
                 print(e)
                 print('Error connecting database')
         return False
+
+    def createInitalDatabse(self, dbPath):
+        connection = sqlite3.connect(dbPath)
+        cursor = connection.cursor()
+        sqlStatementStop = 'CREATE TABLE "TMStopModel" ("uid" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, "relais_id" INTEGER NOT NULL, "mess_id" INTEGER)'
+        sqlStatementSwitch = 'CREATE TABLE "TMSwitchModel" (""uid" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, relais_id" TEXT NOT NULL, "switchType" INTEGER)'
+        cursor.execute(sqlStatementStop)
+        cursor.execute(sqlStatementSwitch)
+        connection.commit()
+        connection.close()
 
     def insertStopModel(self, relaisId, messId):
         if self.openDatabase():
