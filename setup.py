c3947eca4ccb2e123902bb765772f59763184b9c
FelixNievelstein <info@felix-nievelstein.de>
2020-08-18 22:22:18 +0200

    version bumped

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.MD", "r") as fh:
 
 setuptools.setup(
     name='trainmote-module_felix-nievelstein_de',
-    version='0.2.6',
+    version='0.2.7',
     description='Application to create a bluetooth server to control a model train environment',
     long_description=long_description,
     url="https://github.com/FelixNievelstein/Trainmote-Server",
