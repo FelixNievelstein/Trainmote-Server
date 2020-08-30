14bb81d66e4f35e4c68755fa380862401865722f
FelixNievelstein <info@felix-nievelstein.de>
2020-08-30 13:29:50 +0200

    test webserver

--- src/pkg_trainmote/trainmote.py
+++ src/pkg_trainmote/trainmote.py
@@ -1,4 +1,5 @@
 from . import gpioservice
+import flask from Flask
 from .powerControllerModule import PowerThread
 from .configControllerModule import ConfigController
 from . import stateControllerModule
@@ -17,6 +18,8 @@ stateController = stateControllerModule.StateController()
 powerThread = PowerThread()
 client_sock = None
 config = ConfigController()  
+webserver.setup()
+app = Flask(__name__)
 
 def loadPersistentData():
     if config.loadPreferences():
@@ -24,36 +27,30 @@ def loadPersistentData():
             libInstaller = LibInstaller()
             libInstaller.installSQLite()
             if config.setSQLiteInstalled():
-                restart(None, None)
+                restart()
             else: 
-                shutDown(None)
+                shutDown()
+    
+@app.route('/')
+def hello_world():
+    return jsonify(result='Hello World')
+  
 
-# Main loop
-def main():          
-    powerThread.start()
-    loadPersistentData()
-
-
-def restart(server_sock, client_sock):
-    closeClientConnection(client_sock)
-    shutDown(server_sock)
+def restart():
+    shutDown()
     os.execv(sys.executable, ['python'] + sys.argv)
 
-def shutDown(server_sock):
+def shutDown():
     powerThread.kill.set()
     powerThread.isTurningOff = True
     powerThread.join()
     stateController.setState(stateControllerModule.STATE_SHUTDOWN)
-    if server_sock is not None:
-        server_sock.close()
     print ("Server going down")
     stateController.stop()
 
-def closeClientConnection(client_sock):
+def closeClientConnection():
     print ("Closing client socket")
-    if client_sock is not None:
-            client_sock.close()
-            client_sock = None
+    
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True,host="0.0.0.0")  
