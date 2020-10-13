411bd295ec98a689317ed7786701219b946ebfd1
FelixNievelstein <info@felix-nievelstein.de>
2020-10-13 23:09:18 +0200

    test

--- src/pkg_trainmote/gpioservice.py
+++ src/pkg_trainmote/gpioservice.py
@@ -16,7 +16,6 @@ validGpios = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 
 
 def setup():
-    GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     loadInitialData()
@@ -30,7 +29,7 @@ def loadInitialData():
     stopModels = DatabaseController().getAllStopModels()
     for stop in stopModels:
         addRelais(stop)
-    setAllToDefault()
+    #setAllToDefault()
 
 
 def addRelais(relais: GPIORelaisModel):
