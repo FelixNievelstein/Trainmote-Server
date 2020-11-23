7ef2face5a6f414073c78d92576f48fb986716e3
FelixNievelstein <info@felix-nievelstein.de>
2020-11-23 22:27:58 +0100

    fix issue with set autostart;

--- src/pkg_trainmote/trainmote.py
+++ src/pkg_trainmote/trainmote.py
@@ -4,12 +4,12 @@ import os
 import argparse
 
 
-version: str = '0.3.95'
+version: str = '0.3.96'
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "-autostart", help="sets trainmote to start on every device boot.", action="store_true")
+    parser.add_argument("-a", "--autostart", help="sets trainmote to start on every device boot.", action="store_true")
     args = parser.parse_args()
     if args.autostart:
         print("write to rc.local")
@@ -18,11 +18,16 @@ def main():
 
 
 def setAutoStart():
+    lineToAdd = 'sudo trainmote &\n'
+
     with open('/etc/rc.local') as fin:
-        with open('/etc/rc.local.TMP') as fout:
+        with open('/etc/rc.local.TMP', 'w+') as fout:
+            alreadyAdded = False
             for line in fin:
-                if line == 'exit 0':
-                    fout.write('sudo trainmote &\n')
+                if line == lineToAdd:
+                    alreadyAdded = True
+                if line.startswith('exit 0') and not alreadyAdded:
+                    fout.write(lineToAdd)
                 fout.write(line)
             os.rename('/etc/rc.local', '/etc/rc.local.jic')
             os.rename('/etc/rc.local.TMP', '/etc/rc.local')
