9b764eef2886d1f4996485343740603a386708c2
FelixNievelstein <info@felix-nievelstein.de>
2021-03-17 22:09:16 +0100

    #88: adding clock action;

--- src/pkg_trainmote/actions/actionHelper.py
+++ src/pkg_trainmote/actions/actionHelper.py
@@ -1,4 +1,5 @@
 from pkg_trainmote.actions.actionInterface import ActionInterface
+from pkg_trainmote.actions.clockAction import ClockAction, ClockThread
 from pkg_trainmote.models.Action import Action, ActionType
 from pkg_trainmote.actions.setSwitchAction import SetSwitchAction
 from pkg_trainmote.actions.setStopAction import SetStopAction
@@ -12,4 +13,6 @@ def getProgramAction(action: Action) -> Optional[ActionInterface]:
         return SetStopAction(action)
     elif action.type == ActionType.TM_PERFORM_TIMER.value:
         return TimerAction(action)
+    elif action.type == ActionType.TM_PERFORM_ALARM.value:
+        return ClockAction(action)
     return None
--- src/pkg_trainmote/trainmote.py
+++ src/pkg_trainmote/trainmote.py
@@ -6,7 +6,7 @@ from subprocess import call
 from . import configControllerModule
 
 
-version: str = '0.5.16'
+version: str = '0.5.17'
 
 def main():
     parser = argparse.ArgumentParser()
